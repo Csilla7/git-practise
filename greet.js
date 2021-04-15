09ef29bbf72a22dc485cec364459b1826f4aaf8c
Csilla <farkascsillamunka@gmail.com>
2021-04-15 11:12:45 +0200

    sbody changed my function

--- greet.js
+++ greet.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-function greet() {
-    console.log( 'Greetings!');
-}
-=======
 function greet(name) {
     console.log( 'Hello ' + name);
 }
 
 greet('Csilla');
->>>>>>> 7374ae1bec26d1f2dd97fd65879a552f56e78192
